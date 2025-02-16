e747342c1b92bfff0bec731a9c36f5e344501399
이대원 <leedw3232@gmail.com>
2025-02-16 17:11:46 +0900

    NuGet Packages gitignore 위치 변경

---
+++ csharp/test1/WebApplication5/TestProject1/Services/UserServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using TestProject1.unitTest;
+
+namespace TestProject1.Services
+{
+    [TestFixture]
+    public class UserServiceTest
+    {
+        [Test]
+        public void UserTest01()
+        {
+            Console.WriteLine("test");
+
+            // 목 객체 생성
+            var mockRepo = new Mock<IUserRepository>();
+
+            // 목 설정
+            mockRepo.Setup(repo => repo.GetById(1))
+                .Returns(new User { Id = 1, Name = "Test User" });
+
+            // 목 객체 사용
+            var userRepo = mockRepo.Object;
+            var user = userRepo.GetById(1);
+            
+            Assert.AreEqual(user.Id, 2);
+        }
+    }
+}
---
+++ csharp/test1/WebApplication5/TestProject1/unitTest/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestProject1.unitTest
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User()
+        {
+            CreatedAt = DateTime.Now;
+        }
+
+        public User(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
